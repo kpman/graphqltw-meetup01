20643856c3c29c3f40f7f194af85fca71c4cd518
kpman <s92f002@hotmail.com>
2017-08-16 01:00:01 +0800

    Use fake query function

--- README.md
+++ README.md
@@ -2,7 +2,36 @@
 
 A demo project for dataloader with graphql server
 
-## 
+## example query
+```
+{
+  getAuthor(id: 100) {
+    id
+    name
+    posts {
+      id
+      text
+      author {
+        id
+        name
+        posts {
+          id
+          title
+          text
+          author {
+            id
+            posts{
+              id
+              title
+              text
+            }
+          }
+        }
+      }
+    }
+  }
+}
+```
 
 ## License
 
--- src/schema/index.js
+++ src/schema/index.js
@@ -3,6 +3,47 @@ const { makeExecutableSchema } = require('graphql-tools');
 
 let callTimes = 0;
 
+const fakeQueryAuthor = args => {
+  console.log('fakeQueryAuthor', ++callTimes);
+  return Promise.resolve({
+    id: args.id,
+    name: 'kpman',
+  });
+};
+
+const fakeQueryPost = args => {
+  console.log('fakeQueryPost', ++callTimes);
+  return Promise.resolve({
+    id: 2,
+    title: `GraphQL is ${args.titleContains}!`,
+    text: 'GraphQL Taiwan is good!',
+  });
+};
+
+const fakeQueryPostsByAuthor = author => {
+  console.log('fakeQueryPostsByAuthor', ++callTimes);
+  return Promise.resolve([
+    {
+      id: 3,
+      title: `${author.name} blog`,
+      text: 'GraphQL Tawian is good!',
+    },
+    {
+      id: 4,
+      title: `${author.name} loves GraphQL`,
+      text: 'GraphQL Tawian is awesome!',
+    },
+  ]);
+};
+
+const fakeQueryAuthorByPost = post => {
+  console.log('fakeQueryAuthorByPost', ++callTimes);
+  return Promise.resolve({
+    id: 5,
+    name: post.id,
+  });
+};
+
 const typeDefs = `
   type Author {
     id: Int
@@ -25,49 +66,24 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
-    getAuthor() {
-      console.log(++callTimes);
-      return {
-        id: 1,
-        name: 'KP',
-      };
+    getAuthor(obj, args) {
+      return fakeQueryAuthor(args);
     },
 
-    getPostsByTitle() {
-      console.log(++callTimes);
-      return {
-        id: 2,
-        title: 'GraphQL is fun!',
-        text: 'GraphQL Taiwan is good!',
-      };
+    getPostsByTitle(obj, args) {
+      return fakeQueryPost(args);
     },
   },
 
   Author: {
     posts(author) {
-      console.log(++callTimes);
-      return [
-        {
-          id: 3,
-          title: `${author.name} blog`,
-          text: 'GraphQL Tawian is good!',
-        },
-        {
-          id: 4,
-          title: `${author.name} loves GraphQL`,
-          text: 'GraphQL Tawian is awesome!',
-        },
-      ];
+      return fakeQueryPostsByAuthor(author);
     },
   },
 
   Post: {
     author(post) {
-      console.log(++callTimes);
-      return {
-        id: 5,
-        name: post.id,
-      };
+      return fakeQueryAuthorByPost(post);
     },
   },
 };
